befada06d511d88dcd1a90bdca2d36789f7d8a1b
Phan Anh Tuan <96173253+tuanBmo@users.noreply.github.com>
2026-06-02 16:08:06 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// Sử dụng SheetJS để đọc file
+async function loadExcel(file) {
+    const data = await (file.arrayBuffer());
+    const workbook = XLSX.read(data, {type: 'array'});
+    const json = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]]);
+    console.log(json); // Dữ liệu 1000 từ đã được nạp
+}
+
+function flipCard() {
+    // Logic ẩn từ, hiện nghĩa khi click
+}
